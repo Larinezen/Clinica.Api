8c5cb3e372ace3a612f2a2399ce272714960fcf0
Larine Nunes <larine96@outlook.com>
2024-11-30 00:49:28 -0300

    Inclusão de Rota para chamada GEt Inclusão de validações de cpf, email e telefone com a instalação da biblioteca documentValidation

--- Clinica.Api/Controllers/ClinicController.cs
+++ Clinica.Api/Controllers/ClinicController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using DocumentValidation;
 using Clinica.Api.Controllers.Models;
 using System.Data;
 using System.Text.RegularExpressions;
@@ -17,20 +18,37 @@ namespace Clinica.Api.Controllers
         [HttpPost(Name = "SavePeople")]
         public IActionResult SavePeople([FromBody] People people)
         {
-            string cpfFormatado = FormatarCPF(people.Cpf);
+            
             var connection = new SqlConnection(_connectionString);
             connection.Open();
+            // Validação de CPF  com base na biblioteca DocumentValidation instalada
+            // Negação ! -- Se o CPF não for validado retornamos BadRequest
+            if (!people.Cpf.ValidateCpf())
+            {
+                return BadRequest("CPF inválido");
+            }
             // A validação se o CPF já existe na base de dados pode ser feita antes do INSERT
             if (NumeroCpfsEncontrados(people.Cpf) > 0)
             {
                 return BadRequest("Este cpf já existe em nossa base de dados");
             }
-            if(NumeroEmailsEncontrados(people.Email) > 0)
+            // Validação de email  com base na biblioteca DocumentValidation instalada
+            // Negação ! -- Se o email não for validado retornamos BadRequest
+            if (!people.Email.ValidateEmail())
+            {
+                return BadRequest("E-mail inválido");
+            }
+            if (NumeroEmailsEncontrados(people.Email) > 0)
             {
                 return BadRequest("Este endereço de email já está cadastrado em nossa base de dados. Por favor, utilize um email diferente ");
             }
+            if (!people.Tel.ValidatePhone())
+            {
+                return BadRequest("Número de telefone inválido");
+            }
+
             var command = new SqlCommand("INSERT INTO clinica (Cpf, Nome, Nascimento, Email, Tel, Inclusao) VALUES (@Cpf, @Nome, @Nascimento, @Email, @Tel, @Inclusao)", connection);
-            command.Parameters.AddWithValue("@Cpf", cpfFormatado);
+            command.Parameters.AddWithValue("@Cpf", people.Cpf);
             command.Parameters.AddWithValue("@Nome", people.Nome);
             command.Parameters.AddWithValue("@Nascimento", people.Nascimento);
             command.Parameters.AddWithValue("@Email", people.Email);
@@ -50,6 +68,8 @@ namespace Clinica.Api.Controllers
             }                
 
         }
+
+         
         // Método criado para que seja possível consultar em banco de dados quantas cpfs
         // com aquele mesmo número foram encontrados.
         private int NumeroCpfsEncontrados(string NumeroCpf) 
@@ -73,18 +93,11 @@ namespace Clinica.Api.Controllers
             connection.Close();
             return NumeroEmailsEncontrados;
         }
-        private string FormatarCPF(string Cpf)
-        {
-            Cpf = Regex.Replace(Cpf, "[^0-9]", "");
-            if (Cpf.Length != 11)
-                throw new ArgumentException("CPF inválido. Deve conter 11 dígitos.");
-            return Convert.ToUInt64(Cpf).ToString(@"000\.000\.000\-00");
-        }
-
+      
        
         [HttpGet(Name = "GetPeople")]
         public IActionResult Get([FromQuery] string Cpf)
-        {
+        { 
             var connection = new SqlConnection(_connectionString);
             connection.Open();
             var command = new SqlCommand("SELECT * FROM dbo.clinica WHERE Cpf = @NumeroCpf", connection);
@@ -98,7 +111,7 @@ namespace Clinica.Api.Controllers
                 people.Nome = reader.GetString(reader.GetOrdinal("Nome"));
                 people.Nascimento = reader.GetDateTime(reader.GetOrdinal("Nascimento"));
                 people.Email = reader.GetString(reader.GetOrdinal("Email"));
-                people.Tel = reader.GetInt64(reader.GetOrdinal("Tel"));
+                people.Tel = reader.GetString(reader.GetOrdinal("Tel"));
                 listCpf.Add(people);
 
                
@@ -106,13 +119,25 @@ namespace Clinica.Api.Controllers
             connection.Close();
             if(listCpf.Count > 0)
             {
-                return Ok(listCpf.FirstOrDefault());
+                    var response = new
+                {
+                    Message = "CPF localizado com sucesso !",
+                    
+
+                    };
+                    return Ok(response);
 
 
             }
-            else { return NotFound(); }
+            else {
+                var response = new
+                {
+                    Message = "CPF não localizado"
 
-            
+                };
+                return BadRequest(response);
+               
+                }     
         }
     }
 }
